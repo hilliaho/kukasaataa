2ea0ea8b6c58ec1debe88dc5f0eb4d92d71d0d3c
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-02-26 22:00:05 +0200

    Fix submission name display

--- src/frontend/src/components/Submission.jsx
+++ src/frontend/src/components/Submission.jsx
@@ -1,7 +1,7 @@
 const Submission = ({name, type, url}) => {
 	return (
 		<div className="submission">
-			<a href={url}>{type}: {name}</a>
+			{type ? (<a href={url}>{type}: {name}</a>):(<a href={url}>{name}</a>)}
 		</div>
 	)
 }
