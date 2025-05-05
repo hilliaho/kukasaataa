1c47c79aef537e009d34b0bc8e2104e8bb55e952
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-05-05 16:45:18 +0300

    Update app.py to work with gunicorn

--- src/backend/app.py
+++ src/backend/app.py
@@ -7,28 +7,24 @@ CORS(app)
 
 db_service = DBService()
 
-
-
 @app.route("/projects", methods=["GET"])
 def get_data():
     page = int(request.args.get("page", 1))
-    per_page = int(request.args.get("per_page", 10))
+    per_page = int(request.args.get("per_page", 1))
     search_query = request.args.get("search_query", "")
     try:
         data = db_service.fetch_documents(page, per_page, search_query)
         return jsonify(data), 200
     except Exception as e:
+        print(f"Error fetching data: {e}")
         return jsonify({"error": "Failed to fetch data"}), 500
     
 @app.route("/projects/count", methods=["GET"])
 def get_result_count():
     search_query = request.args.get("search_query", "")
     try:
-      total_count = db_service.count_documents(search_query)
-      print("app.py total count:", total_count)
-      return jsonify({"count": total_count}), 200
+        total_count = db_service.count_documents(search_query)
+        print("app.py total count:", total_count)
+        return jsonify({"count": total_count}), 200
     except Exception as e:
-      return jsonify({"error": "Failed to get results count"}), 500
-
-if __name__ == "__main__":
-  app.run(debug=True)
+        return jsonify({"error": "Failed to get results count"}), 500
