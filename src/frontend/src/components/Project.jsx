d8939114ab87a80b8d0ac31f3f6230603c96e228
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-05-06 14:32:30 +0300

    Remove console log

--- src/frontend/src/components/Project.jsx
+++ src/frontend/src/components/Project.jsx
@@ -2,8 +2,6 @@ import React from "react"
 import DocumentInfo from "./DocumentInfo"
 
 const Project = ({project}) => {
-
-	console.log("project: ", project)
 	return (
 		<div className="result-item">
 			<p><strong>HANKE {project.heTunnus}</strong></p>
