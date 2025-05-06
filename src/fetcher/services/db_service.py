010f776fc60c609b4fe11fcf14b42c480b2be856
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-05-06 12:26:21 +0300

    Remove unused functions

--- src/fetcher/services/db_service.py
+++ src/fetcher/services/db_service.py
@@ -14,38 +14,19 @@ class DBService:
         self.db = self.client["kukasaataa"]
         self.collection = self.db["projects"]
 
+    def add_preparatory_id(self, he_id, preparatory_id):
+        result = self.collection.update_one(
+            {"heTunnus": he_id},
+            {"$set": {"valmistelutunnus": preparatory_id}},
+        )
+        return result.modified_count
+    
     def add_document(self, document):
         result = self.collection.insert_one(document)
         return result.inserted_id
-
+    
     def document_exists(self, document_id):
         return self.collection.find_one({"id": document_id}) is not None
-
-    def find_document_by_he_id(self, he_id):
-        return self.collection.find_one({"heTunnus": he_id})
-
-    def update_document(self, he_id, preparatory_id, submissions):
-        result = self.collection.update_one(
-            {"heTunnus": he_id},
-            {"$set": {"valmistelutunnus": preparatory_id, "dokumentit.lausunnot": submissions}},
-        )
-        return result.modified_count
-
-    def clean_identifiers(self):
-        regex = r" vp$"
-        documents = self.collection.find({"heTunnus": {"$regex": regex}})
-        updated_count = 0
-
-        for doc in documents:
-            identifier = doc["heTunnus"]
-            new_identifier = re.sub(regex, "", identifier)
-            print(f"{identifier} -> {new_identifier}")
-            self.collection.update_one(
-                {"_id": doc["_id"]}, {"$set": {"heTunnus": new_identifier}}
-            )
-            updated_count += 1
-
-        print(f"Päivitetty {updated_count} dokumenttia")
 
     def create_search_index(self):
         existing_indexes = self.collection.list_indexes()
@@ -62,22 +43,12 @@ class DBService:
         else:
             print("Tekstihakemisto on jo olemassa.")
 
-    def push_documents(self, data, he_id, document_type):
+    def push_document(self, data, he_id, document_type):
         print(he_id)
         result = self.collection.update_one(
             {"heTunnus": he_id}, {"$addToSet": {f"dokumentit.{document_type}": data}}
         )
         return result.modified_count
-
-    def delete_documents(self, document_type):
-        i = 0
-        for doc in self.collection.find():
-            print(i)
-            i += 1
-            result = self.collection.update_one(
-                {"_id": doc["_id"]}, {"$set": {f"dokumentit.{document_type}": []}}
-            )
-            print("modified count: ", result.modified_count)
 
     def count_documents(self, search_query=""):
         query = {}
