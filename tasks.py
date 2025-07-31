48806aac8f8fe9af1084687549ff2fdaddd8bb29
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-07-31 13:55:20 +0300

    Add poetry task to run fetcher

--- tasks.py
+++ tasks.py
@@ -1,14 +1,19 @@
 from invoke import task
 
-
 @task
 def backend(ctx):
     with ctx.cd("src/backend"):
-        ctx.run("source venv/bin/activate", pty=True)
-        ctx.run("python3 app.py", pty=True)
-
+        ctx.run("poetry install", pty=True)
+        ctx.run("poetry run python3 app.py", pty=True)
 
 @task
 def frontend(ctx):
     with ctx.cd("src/frontend"):
         ctx.run("npm start", pty=True)
+
+@task
+def fetcher(ctx):
+    with ctx.cd("src/fetcher"):
+        ctx.run("poetry install", pty=True)
+        ctx.run("poetry run python3 main.py", pty=True)
+
