2eb05508035a30623018f60c8b09a2d80de593bd
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-08-25 16:06:21 +0300

    Change government proposal name text to link in document selection view

--- src/frontend/src/components/Project.jsx
+++ src/frontend/src/components/Project.jsx
@@ -1,16 +1,17 @@
 import React from "react"
 import DocumentInfo from "./DocumentInfo"
 
-const Project = ({project, setSelectedProjects}) => {
+const Project = ({ project, setSelectedProjects }) => {
+
 	return (
 		<div className="project-item">
 			<p><strong>HANKE {project.heTunnus}</strong></p>
-			<p>{project.heNimi}</p>
+			<a href={project.heUrl}>{project.heNimi}</a>
 			<ul>
-				{<DocumentInfo projectId={project._id} objectName={"lausunnot"} header={"Lausuntokierroksen lausunnot"} submissions={project.dokumentit.lausunnot ?? []} setSelectedProjects={setSelectedProjects}/>}
-				{<DocumentInfo projectId={project._id} objectName={"asiantuntijalausunnot"} header={"Asiantuntijalausunnot"} submissions={project.dokumentit.asiantuntijalausunnot ?? []} setSelectedProjects={setSelectedProjects}/>}
-				{<DocumentInfo projectId={project._id} objectName={"valiokunnanLausunnot"} header={"Valiokunnan lausunnot"} submissions={project.dokumentit.valiokunnanLausunnot ?? []} setSelectedProjects={setSelectedProjects}/>}
-				{<DocumentInfo projectId={project._id} objectName={"valiokunnanMietinnot"} header={"Valiokunnan mietinnöt"} submissions={project.dokumentit.valiokunnanMietinnot ?? []} setSelectedProjects={setSelectedProjects}/>}
+				{<DocumentInfo projectId={project._id} objectName={"lausunnot"} header={"Lausuntokierroksen lausunnot"} submissions={project.dokumentit.lausunnot ?? []} setSelectedProjects={setSelectedProjects} />}
+				{<DocumentInfo projectId={project._id} objectName={"asiantuntijalausunnot"} header={"Asiantuntijalausunnot"} submissions={project.dokumentit.asiantuntijalausunnot ?? []} setSelectedProjects={setSelectedProjects} />}
+				{<DocumentInfo projectId={project._id} objectName={"valiokunnanLausunnot"} header={"Valiokunnan lausunnot"} submissions={project.dokumentit.valiokunnanLausunnot ?? []} setSelectedProjects={setSelectedProjects} />}
+				{<DocumentInfo projectId={project._id} objectName={"valiokunnanMietinnot"} header={"Valiokunnan mietinnöt"} submissions={project.dokumentit.valiokunnanMietinnot ?? []} setSelectedProjects={setSelectedProjects} />}
 			</ul>
 		</div>
 	)
