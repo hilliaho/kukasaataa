302fcf34b781ea6dac42ec7ce5be696860ba369e
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-09-24 12:53:52 +0300

    Make hankeikkuna fetch faster by changing for loop to filtering api results with last modified date

--- src/fetcher/services/hankeikkuna_api_service.py
+++ src/fetcher/services/hankeikkuna_api_service.py
@@ -1,46 +1,48 @@
 import requests
 import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class HankeikkunaApiService:
 
-    def fetch_data_from_api(per_page: int, page: int):
+    def fetch_data_from_api(per_page: int, last_modified: str, search_after: str):
         request_url = "https://api.hankeikkuna.fi/api/v2/kohteet/haku"
-        for i in range(page):
-            headers = {
-                "accept": "application/json;charset=UTF-8",
-                "Content-Type": "application/json",
+        headers = {
+            "accept": "application/json;charset=UTF-8",
+            "Content-Type": "application/json",
+        }
+        if not search_after:
+            query = {
+                "muokattuPaivaAlku": last_modified,
+                "size": per_page,
+                "sort": [
+                    {
+                        "field": "kohde.muokattu",
+                        "order": "DESC",
+                    }
+                ],
+                "tyyppi": ["LAINSAADANTO"],
             }
-            if i == 0:
-                query = {
-                    "muokattuPaivaAlku": "2015-01-01T00:00:00",
-                    "size": per_page,
-                    "sort": [
-                        {
-                            "field": "kohde.muokattu",
-                            "order": "DESC",
-                        }
-                    ],
-                    "tyyppi": ["LAINSAADANTO"],
-                }
-            else:
-                query = {
-                    "muokattuPaivaAlku": "2015-01-01T00:00:00",
-                    "searchAfter": data["nextSearchAfter"],
-                    "size": per_page,
-                    "sort": [
-                        {
-                            "field": "kohde.muokattu",
-                            "order": "DESC",
-                        }
-                    ],
-                    "tyyppi": ["LAINSAADANTO"],
-                }
-            response = requests.post(
-                request_url, headers=headers, data=json.dumps(query)
-            )
-            if response.status_code == 200:
-                data = response.json()
-            else:
-                print("Fetching data failed:", response.status_code)
+        else:
+            query = {
+                "muokattuPaivaAlku": last_modified,
+                "searchAfter": search_after,
+                "size": per_page,
+                "sort": [
+                    {
+                        "field": "kohde.muokattu",
+                        "order": "DESC",
+                    }
+                ],
+                "tyyppi": ["LAINSAADANTO"],
+            }
+        response = requests.post(
+            request_url, headers=headers, data=json.dumps(query)
+        )
+        if response.status_code == 200:
+            data = response.json()
+        else:
+            logger.error("Fetching data failed:", response.status_code)
         return data
